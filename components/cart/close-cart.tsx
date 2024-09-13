1c54d7a20a65d0678206e92f6a409108b27b3cda
Arman Abkar <armanabkar@gmail.com>
2024-09-13 13:41:46 +0330

    Small cleanup

--- components/cart/close-cart.tsx
+++ components/cart/close-cart.tsx
@@ -6,7 +6,7 @@ export default function CloseCart({ className }: { className?: string }) {
     <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
       <XMarkIcon
         className={clsx(
-          "h-6 transition-all ease-in-out hover:scale-110 ",
+          "h-6 transition-all ease-in-out hover:scale-110",
           className
         )}
       />
